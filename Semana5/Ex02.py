0cbe5f8e7eff130a5085a882d372178c3492a7b3
talissonbugsf <talissonbugsf@gmail.com>
2026-04-07 22:18:22 -0300

    Update Ex02.py

--- Semana5/Ex02.py
+++ Semana5/Ex02.py
@@ -1,1 +1,15 @@
-dasdasdadas
+codigo_brinquedo = 1
+carrinho = 0
+
+while codigo_brinquedo != 0:
+    
+    codigo_brinquedo = int(input("Digite o código do produto: "))
+     
+    if codigo_brinquedo == 1040:
+        
+        carrinho = carrinho + 1
+        print(" Quantidade de vezes que o código 1040 foi digitado: ", carrinho)
+
+    elif codigo_brinquedo == 0:
+        print("Processo encerrado")
+        print("Quantidade de vezes que o código 1040 foi digitado: ", carrinho)
