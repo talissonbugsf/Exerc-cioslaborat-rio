acdbe573ae7c96aeb5d07b8f17f1aee3d0dc05a4
talissonbugsf <talissonbugsf@gmail.com>
2026-04-14 20:22:34 -0300

    Update Ex05.py

--- Semana6/Ex05.py
+++ Semana6/Ex05.py
@@ -1,1 +1,5 @@
+numero_oliveiras = int(input("Digite a quantidade de oliveiras plantadas:"))
 
+azeite = numero_oliveiras / 20
+
+print("A quantidade de azeite produzido no ano é:", azeite, "litros")
