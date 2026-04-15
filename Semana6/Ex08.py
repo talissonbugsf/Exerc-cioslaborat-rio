02dd55f6f32eca6187b97725e780f954ece7c77f
talissonbugsf <talissonbugsf@gmail.com>
2026-04-14 21:01:17 -0300

    Update Ex08.py

--- Semana6/Ex08.py
+++ Semana6/Ex08.py
@@ -1,1 +1,12 @@
-a
+plantacao_A = 80000
+plantacao_B = 200000
+anos = 0
+
+while plantacao_A < plantacao_B:
+    
+    plantacao_A += plantacao_A * 0.03
+    plantacao_B += plantacao_B * 0.015
+    
+    anos += 1
+    
+print("A quantidade de anos para a plantação A ultrapassar a B é:", anos)
