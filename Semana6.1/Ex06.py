4b5820c9579051c6ba6df86fce3cb33ee98b0382
talissonbugsf <talissonbugsf@gmail.com>
2026-04-15 12:44:19 -0300

    Create Ex06.py

---
+++ Semana6.1/Ex06.py
@@ -0,0 +1,9 @@
+preco = 0
+contador = 1
+print("Loja das Oliveiras – Tabela de preços")
+
+while contador != 51:
+    preco = preco + 1.99
+    print(contador, " - R$ ", preco)
+  
+    contador += 1
