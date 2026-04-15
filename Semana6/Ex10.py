d7d2f07f8bee4e2354e394540ca08c0458caa753
talissonbugsf <talissonbugsf@gmail.com>
2026-04-14 21:19:09 -0300

    Update Ex10.py

--- Semana6/Ex10.py
+++ Semana6/Ex10.py
@@ -1,1 +1,12 @@
-aaa
+meu_numero = int(input("Digite seu número (DE 1 a 100):"))
+usuario_numero = 0
+
+while meu_numero != usuario_numero:
+    usuario_numero = int(input("Digite o número do usuário (DE 1 a 100):"))
+    
+    if usuario_numero < meu_numero:
+        print("Há mais oliveiras, tente um número maior")
+    elif usuario_numero > meu_numero:
+        print("Há menos oliveiras, tente um número menor")
+    elif meu_numero == usuario_numero:
+        print("Parabéns! Você descobriu a quantidade de oliveiras!")
