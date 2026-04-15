f27b25123d7daa9014499a46be8813332e57ecd8
talissonbugsf <talissonbugsf@gmail.com>
2026-04-14 21:57:22 -0300

    Create Ex02.py

---
+++ Semana6.1/Ex02.py
@@ -0,0 +1,10 @@
+ciclo = 0
+total = 500
+gastos = 30
+
+while total >= 0:
+    total = total - gastos
+    
+    ciclo += 1
+    
+print("A quantidade de ciclos feitos até o reservatório ficar vazio é:", ciclo)
