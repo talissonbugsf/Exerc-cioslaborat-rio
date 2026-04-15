d38163f04a369667d8daecafbf9d61c597779f7b
talissonbugsf <talissonbugsf@gmail.com>
2026-04-14 21:26:27 -0300

    Update Ex08.py

--- Semana6/Ex08.py
+++ Semana6/Ex08.py
@@ -2,7 +2,7 @@ plantacao_A = 80000
 plantacao_B = 200000
 anos = 0
 
-while plantacao_A < plantacao_B:
+while plantacao_A <= plantacao_B:
     
     plantacao_A += plantacao_A * 0.03
     plantacao_B += plantacao_B * 0.015
