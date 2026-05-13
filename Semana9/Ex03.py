8cbf42d2294bb599de832ea2a339b312d3967c65
talissonbugsf <talissonbugsf@gmail.com>
2026-05-13 15:37:28 -0300

    Update Ex03.py

--- Semana9/Ex03.py
+++ Semana9/Ex03.py
@@ -1,1 +1,9 @@
+primeiro_numero = 0
+outro_numero = 1
 
+for contador in range(1, 11):
+    print(contador, primeiro_numero)
+    
+    fibonacci = primeiro_numero + outro_numero
+    primeiro_numero = outro_numero
+    outro_numero = fibonacci
