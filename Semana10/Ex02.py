4491c2a79351ac1d81b22a6ddf9a38616f9686bc
talissonbugsf <talissonbugsf@gmail.com>
2026-05-21 16:10:03 -0300

    Update Ex02.py

--- Semana10/Ex02.py
+++ Semana10/Ex02.py
@@ -1,1 +1,20 @@
+valor = []
+maior_100 = 0
+
+for v in range (0, 10):
+    numero = float(input("Digite o número:"))
+    valor.append(numero)
+
+    if numero > 100:
+        maior_100 += 1
+    else:
+        pass
+
+print("Quantos números são maiores que 100:", maior_100)
+
+for v in range (len(valor)):
+    if valor[v] > 100:
+        print("Valor ", v + 1, "=", valor[v])
+
+
 
