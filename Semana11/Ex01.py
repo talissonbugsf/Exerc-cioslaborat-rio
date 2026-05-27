42b271a7566436f99d27870af8f8763dc43a2fee
talissonbugsf <talissonbugsf@gmail.com>
2026-05-26 21:56:21 -0300

    Update Ex01.py

--- Semana11/Ex01.py
+++ Semana11/Ex01.py
@@ -1,1 +1,20 @@
+def notas(nota1, nota2):
+    media_notas = (nota1 + nota2) / 2
+    return media_notas
 
+def situacao(media_notas):
+    if media_notas >= 7:
+        print("Você foi aprovado!")
+    else:
+        print("Você foi reprovado!")
+
+def main():
+    nota1 = float(input("Digite a primeira nota:"))
+    nota2 = float(input("Digite a segunda nota:"))
+    media = notas(nota1, nota2)
+    situacao(media)
+    print("Média:", media)
+    print(situacao)
+
+
+main()
