fd513fe1a5509be7323d8cf759c58aada8629626
talissonbugsf <talissonbugsf@gmail.com>
2026-05-27 14:56:02 -0300

    Update Ex05.py

--- Semana11/Ex05.py
+++ Semana11/Ex05.py
@@ -1,1 +1,27 @@
+def aprovado (media):
+    print("Aprovado!")
 
+def recuperacao (media):
+    print("Recuperação!")
+
+def reprovado (media):
+    print("Reprovado")
+
+def main():
+    soma_notas = 0
+    for n in range(5):
+        notas = float(input("Digite a nota:"))
+        soma_notas = soma_notas + notas
+    media = soma_notas / 5
+    print("Média final:", media)
+
+    if media >= 7:
+        aprovado (media)
+    
+    elif media >= 4 and media < 7:
+        recuperacao (media)
+
+    elif media < 4:
+        reprovado (media)
+
+main()
