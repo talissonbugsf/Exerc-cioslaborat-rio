825e3c4526383370dc7586cb08188d7a817aa46b
talissonbugsf <talissonbugsf@gmail.com>
2026-05-26 22:35:13 -0300

    Update Ex04.py

--- Semana11/Ex04.py
+++ Semana11/Ex04.py
@@ -1,1 +1,17 @@
+def laranxinha(laranjas_compradas):
 
+    if laranjas_compradas <= 12:
+        la = laranjas_compradas * 0.40
+        return la
+    
+    elif laranjas_compradas > 12:
+        la = laranjas_compradas * 0.25
+        return la
+
+def main():
+    laranjas_compradas = int(input("Quantidades de laranjas compradas:"))
+    laranxinha(laranjas_compradas)
+    la = laranxinha(laranjas_compradas)
+    print("Valor da compra: R$ ", la)
+
+main()
