58f5772d4154b02ecf1939f20fa158b066890c26
talissonbugsf <talissonbugsf@gmail.com>
2026-06-10 16:52:27 -0300

    Update Ex01.py

--- Semana12.1/Ex01.py
+++ Semana12.1/Ex01.py
@@ -1,1 +1,24 @@
+def leitura():
+    lotes = []
 
+    for v in range(5):
+        lote = int(input("Valor de lote:"))
+        lotes.append(lote)
+
+    print("Ordem Original:")
+    
+    for i in range(5):
+        print(lotes[i])
+    
+    return lotes
+
+def inverso(lotes):
+    print("Ordem contrária:")
+    for v in range(4, -1, -1):
+        print(lotes[v]) 
+
+def main():
+    lotes = leitura()
+    inverso(lotes)
+
+main()
