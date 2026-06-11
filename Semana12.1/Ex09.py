880ceb3922f8790e85f34f299feaeb0b482d70b0
talissonbugsf <talissonbugsf@gmail.com>
2026-06-11 14:37:55 -0300

    Update Ex09.py

--- Semana12.1/Ex09.py
+++ Semana12.1/Ex09.py
@@ -1,1 +1,30 @@
+import random
 
+def leitura():
+    codigos = []
+
+    for v in range(10):
+        codigo = random.randint(1, 100)
+        codigos.append(codigo)
+
+    print("Números escolhidos:", codigos)
+
+    return codigos
+
+def pares(codigos):
+    pares = 0
+
+    for codigo in codigos:
+        if codigo % 2 == 0:
+            pares += 1
+        else:
+            pass
+
+    print("Números pares:", pares)
+
+
+def main():
+    codigos = leitura()
+    pares(codigos)
+
+main()
